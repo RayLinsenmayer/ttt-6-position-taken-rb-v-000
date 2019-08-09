a6e23d2bcd7c38edccee7d8fea29cab8f7090c26
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2019-08-09 15:43:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,26 @@
-
-
-def position_taken?(board, index)
-
-if board[index] == "" || board[index] == " "
-    return false
-  elsif board[index] == nil
-    return false
-  else
-    return true
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+board = [" "," "," "," "," "," "," "," "]
+
+def input_to_index(user_input)
+  user_input.to_i-1
 end
+
+def move(board, position, char = "X")
+  board[position] = char
+end
+
+def position_taken?(board, position)
+  if board[position] == " " || board[position]== "" || board[position] == nil
+    return false
+  else return true
+end
+end
+
+
